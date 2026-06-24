7b4ca01f9b318ef4ebb900443cf7a93c2430f6e1
Dave Hagedorn <dave.hagedorn@textnow.com>
2026-06-24 15:36:19 -0400

    Restore Call.reconnect() wrapper dropped by 27b8a24 (Swift wrapper generation was disabled in that commit)

--- Sources/linphonesw/LinphoneWrapper.swift
+++ Sources/linphonesw/LinphoneWrapper.swift
@@ -12385,6 +12385,14 @@ public class Call : LinphoneObject
 	
 	
 	
+	/// Causes a running call to reconnect, the same way as a network reconnect. 
+	public func reconnect() 
+	{
+		linphone_call_reconnect(cPtr)
+	}
+	
+	
+	
 	/// Redirect the specified call to the given redirect URI. 
 	/// - Parameter redirectUri: The URI to redirect the call to    
 	/// - Returns: 0 if successful, -1 on error. 
